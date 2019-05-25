116edea877fd10817c467ebb8a2927d531254fb8
胡敏 <786242793@qq.com>
2019-05-25 14:52:39 +0800

    实体类最新

--- src/main/java/com/car_insurance/entity/RoleExample.java
+++ src/main/java/com/car_insurance/entity/RoleExample.java
@@ -4,5 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RoleExample extends Role{
-
+    public RoleExample(Integer role_id, String role_name) {
+        super(role_id, role_name);
+    }
 }
