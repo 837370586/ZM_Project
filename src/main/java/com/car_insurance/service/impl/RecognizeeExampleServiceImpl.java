06ffb3e2eb94480d3014271b7dc469d6032d30a8
胡敏 <786242793@qq.com>
2019-05-26 09:24:28 +0800

    已补全

--- src/main/java/com/car_insurance/service/impl/RecognizeeExampleServiceImpl.java
+++ src/main/java/com/car_insurance/service/impl/RecognizeeExampleServiceImpl.java
@@ -1,11 +1,16 @@
 package com.car_insurance.service.impl;
 
+import com.car_insurance.entity.Recognizee;
+import com.car_insurance.entity.RecognizeeExample;
 import com.car_insurance.mapper.RecognizeeExampleMapper;
 import com.car_insurance.service.RecognizeeExampleService;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.List;
+/*
+* 胡敏
+* */
 @Service
 public class RecognizeeExampleServiceImpl implements RecognizeeExampleService {
 
@@ -13,7 +18,19 @@ public class RecognizeeExampleServiceImpl implements RecognizeeExampleService {
     private RecognizeeExampleMapper  pem;
 
 
-    public List<RecognizeeExample> selectAllRecognizeeExample() {
-        return pem.selectAllRecognizeeExample();
+    public List<RecognizeeExample> selectAllRecognizeeExample(String carid) {
+
+        return pem.selectAllRecognizeeExample(carid);
+    }
+
+    @Override
+    public void addWorkorder() {
+
+    }
+
+
+    @Override
+    public List<Recognizee> selectAllRecognizee(String applicant_idcard) {
+        return null;
     }
 }
