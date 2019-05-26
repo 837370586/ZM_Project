8d665efa72e134b32d15ba2d88e6a73882cb7457
胡敏 <786242793@qq.com>
2019-05-26 09:08:24 +0800

    最新2

--- src/main/java/com/car_insurance/service/RecognizeeExampleService.java
+++ src/main/java/com/car_insurance/service/RecognizeeExampleService.java
@@ -10,6 +10,7 @@ import java.util.List;
 public interface RecognizeeExampleService {
     //查询所有的方法，主要是通过客户提供的车牌号查投保信息
     List<RecognizeeExample> selectAllRecognizeeExample(String carid);
+
     //生成订单的方法
     public  void addWorkorder();
 
