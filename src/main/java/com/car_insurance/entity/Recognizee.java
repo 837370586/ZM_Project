44c6edd3a1094ae4373ab309c44edea05e02743e
胡敏 <786242793@qq.com>
2019-05-26 08:46:26 +0800

    最新

--- src/main/java/com/car_insurance/entity/Recognizee.java
+++ src/main/java/com/car_insurance/entity/Recognizee.java
@@ -35,25 +35,6 @@ public class Recognizee {
 
     private Date recognizee;
 
-    public Recognizee(Integer recognizee_id, String recognizee_idcard, String name, Integer age, String sex, String phone, Integer applicant_id, String address, String carid, String car_category, String engine, String vin, Integer insurance_id, String policynumber, String car_owner, Date recognizee) {
-        this.recognizee_id = recognizee_id;
-        this.recognizee_idcard = recognizee_idcard;
-        this.name = name;
-        this.age = age;
-        this.sex = sex;
-        this.phone = phone;
-        this.applicant_id = applicant_id;
-        this.address = address;
-        this.carid = carid;
-        this.car_category = car_category;
-        this.engine = engine;
-        this.vin = vin;
-        this.insurance_id = insurance_id;
-        this.policynumber = policynumber;
-        this.car_owner = car_owner;
-        this.recognizee = recognizee;
-    }
-
     public Integer getRecognizee_id() {
         return recognizee_id;
     }
@@ -179,6 +160,25 @@ public class Recognizee {
     }
 
     public void setRecognizee(Date recognizee) {
+        this.recognizee = recognizee;
+    }
+
+    public Recognizee(Integer recognizee_id, String recognizee_idcard, String name, Integer age, String sex, String phone, Integer applicant_id, String address, String carid, String car_category, String engine, String vin, Integer insurance_id, String policynumber, String car_owner, Date recognizee) {
+        this.recognizee_id = recognizee_id;
+        this.recognizee_idcard = recognizee_idcard;
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+        this.phone = phone;
+        this.applicant_id = applicant_id;
+        this.address = address;
+        this.carid = carid;
+        this.car_category = car_category;
+        this.engine = engine;
+        this.vin = vin;
+        this.insurance_id = insurance_id;
+        this.policynumber = policynumber;
+        this.car_owner = car_owner;
         this.recognizee = recognizee;
     }
 }
--- src/main/java/com/car_insurance/entity/RecognizeeExample.java
+++ src/main/java/com/car_insurance/entity/RecognizeeExample.java
@@ -5,8 +5,10 @@ import java.util.Date;
 import java.util.List;
 
 public class RecognizeeExample extends  Recognizee{
+    private  Applicant applicant;
 
-    public RecognizeeExample(Integer recognizee_id, String recognizee_idcard, String name, Integer age, String sex, String phone, Integer applicant_id, String address, String carid, String car_category, String engine, String vin, Integer insurance_id, String policynumber, String car_owner, Date recognizee) {
+    public RecognizeeExample(Integer recognizee_id, String recognizee_idcard, String name, Integer age, String sex, String phone, Integer applicant_id, String address, String carid, String car_category, String engine, String vin, Integer insurance_id, String policynumber, String car_owner, Date recognizee, Applicant applicant) {
         super(recognizee_id, recognizee_idcard, name, age, sex, phone, applicant_id, address, carid, car_category, engine, vin, insurance_id, policynumber, car_owner, recognizee);
+        this.applicant = applicant;
     }
 }
